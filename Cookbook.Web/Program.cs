7f4100b3c2c6bc825c5eca80cb5a52b2f9bd0d51
Ben Hyer <benhyer333@gmail.com>
2026-03-16 16:28:47 -0600

    added entra external id to apphost

--- Cookbook.Web/Program.cs
+++ Cookbook.Web/Program.cs
@@ -1,5 +1,6 @@
 using Cookbook.Web;
 using Cookbook.Web.Components;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +19,9 @@ builder.Services.AddHttpClient<WeatherApiClient>(client =>
         // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
         client.BaseAddress = new("https+http://apiservice");
     });
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
 
 var app = builder.Build();
 
