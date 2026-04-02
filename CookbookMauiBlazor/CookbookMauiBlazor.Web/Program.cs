390677836339bddb9c23ff4a365c3d380c38c2c5
marc.semadeni <marc.semadeni@ENGR9HCP5S3>
2026-04-01 18:16:18 -0600

    redirect in development only

--- CookbookMauiBlazor/CookbookMauiBlazor.Web/Program.cs
+++ CookbookMauiBlazor/CookbookMauiBlazor.Web/Program.cs
@@ -104,7 +104,10 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthentication();
 app.UseAuthorization();
