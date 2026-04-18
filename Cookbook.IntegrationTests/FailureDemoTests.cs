46581ed4e4b95abb8f5684c1be1faa68bdb13ec3
Marc Semadeni <marc.semadeni@students.snow.edu>
2026-04-18 16:48:38 -0600

    forgot to skip the test

--- Cookbook.IntegrationTests/FailureDemoTests.cs
+++ Cookbook.IntegrationTests/FailureDemoTests.cs
@@ -10,7 +10,7 @@ public class FailureDemoTests : IClassFixture<DatabaseFixture>
         _fixture = fixture;
     }
 
-    [Fact]
+    [Fact(Skip = "demo only")]
     public async Task Demo_IntentionalFailure_BoardNameShouldNotBeEmpty()
     {
         await using var db = _fixture.CreateContext();
