43a7f518626801da49f04704d40848b6558a541d
Marc Semadeni <marc.semadeni@students.snow.edu>
2026-04-27 12:16:14 -0600

    Added asyncInterface and fixing tests

--- CookbookMauiBlazor/CookbookMauiBlazor/Platforms/Android/MainActivity.cs
+++ CookbookMauiBlazor/CookbookMauiBlazor/Platforms/Android/MainActivity.cs
@@ -9,7 +9,7 @@ namespace CookbookMauiBlazor
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
-        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent? data)
         {
             base.OnActivityResult(requestCode, resultCode, data);
             AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(
